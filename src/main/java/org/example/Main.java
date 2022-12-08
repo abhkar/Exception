516ee4131f5e4045f2333393fae2bcd6efbe2498
abhishekmalakar <malakar.abh@gmail.com>
2022-12-09 00:12:22 +0530

    changes done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a=5;
+        int b=0;
+       // try-catch block
+        try{
+            System.out.println(a/b);
+        }catch(ArithmeticException e){
+            //e.printStackTrace();
+            System.out.println("This is an arithmetic exception !!");
+        }
     }
 }
